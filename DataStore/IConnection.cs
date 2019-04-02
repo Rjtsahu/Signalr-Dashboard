0d46fbfc6b0d0686a6e36c34f3e3766df2ae2e06
Rajat Sahu <sahurjt@gmail.com>
2019-04-02 23:00:28 +0530

    added common data op method in interface

--- DataStore/IConnection.cs
+++ DataStore/IConnection.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sahurjt.Signalr.Dashboard.DataStore
+﻿namespace Sahurjt.Signalr.Dashboard.DataStore
 {
     interface IConnection
     {
+        void Connect();
+        void Disconnect();
     }
 }
--- DataStore/IDataWriter.cs
+++ DataStore/IDataWriter.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Sahurjt.Signalr.Dashboard.DataStore
 {
-    interface IDataWriter
+    interface IDataWriter<T>
     {
+        void Add(T data);
+        bool Update(string id, T data);
+        bool Delete(string id);
     }
 }
