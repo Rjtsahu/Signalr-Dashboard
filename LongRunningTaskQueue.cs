5226a691dd6fedc5a874d4c297c06c0e5f4462f5
rajat.sahu <rajat.sahu@nineleaps.com>
2019-06-04 16:16:41 +0530

    added LongRunningTaskQueue class to process intercepting operation in background synchronously

---
+++ LongRunningTaskQueue.cs
@@ -0,0 +1,49 @@
+﻿using Sahurjt.Signalr.Dashboard.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sahurjt.Signalr.Dashboard
+{
+    class LongRunningTaskQueue
+    {
+        //  wait for 1 ms before checking if a new task is available to execute
+        private static readonly int _loopWaitTime = 1;
+        private static bool _isQueueEmpty = true;
+        private static readonly Queue<Task> _tasks = new Queue<Task>();
+
+        public static void Enqueue(Action operation)
+        {
+            if (operation == null) throw new ArgumentNullException("Operation delegate must not be null");
+
+            _tasks.Enqueue(new Task(operation));
+
+            LogHelper.Log(" task enqueued");
+
+            if (_isQueueEmpty)
+            {
+                _isQueueEmpty = false;
+                ProcessTaskQueue();
+            }
+        }
+
+        private static void ProcessTaskQueue()
+        {
+            while (!_isQueueEmpty)
+            {
+                if (_tasks.Count > 0)
+                {
+                    var currentTask = _tasks.Dequeue();
+
+                    currentTask.RunSynchronously();
+                    currentTask.Wait();
+                }
+                else
+                {
+                    _isQueueEmpty = true;
+                }
+                Task.Delay(_loopWaitTime);
+            }
+        }
+    }
+}
--- Middleware/SignalrDashboardMiddleware.cs
+++ Middleware/SignalrDashboardMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Owin;
@@ -37,5 +38,6 @@ namespace Sahurjt.Signalr.Dashboard.Middleware
 
             LogHelper.Log("exiting");
         }
+
     }
 }
