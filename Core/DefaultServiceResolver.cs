caa8cca51b8b6715ae537900e8da56cd3fb238ef
rajat.sahu <rajat.sahu@nineleaps.com>
2019-06-19 21:46:18 +0530

    added dep resolver class in core namespace

---
+++ Core/DefaultServiceResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sahurjt.Signalr.Dashboard.Core
+{
+    class DefaultServiceResolver : IServiceResolver
+    {
+        private readonly Dictionary<Type, Func<object>> _resolver = new Dictionary<Type, Func<object>>();
+
+        public TInterface GetService<TInterface>()
+        {
+            if (_resolver.TryGetValue(typeof(TInterface),out var activator)) {
+
+                var obj =  activator();
+                return (TInterface) obj;
+            }
+           
+                throw new NotImplementedException();
+        }
+
+        public void Register(Type serviceType, Func<object> activator)
+        {
+            if (!serviceType.IsInterface)
+            {
+                throw new ArgumentException("Service type must be an interface.");
+            }
+
+            if (_resolver.ContainsKey(serviceType))
+            {
+                throw new Exception("The object intitalizer is already registered for this interface.");
+            }
+
+            _resolver.Add(serviceType, activator);
+        }
+    }
+}
---
+++ Core/IServiceResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sahurjt.Signalr.Dashboard.Core
+{
+    internal interface IServiceResolver
+    {
+        TInterface GetService<TInterface>();
+
+        void Register(Type serviceType, Func<object> activator);
+    }
+}
