316efd010430eeb82abf22f7ca73a8012d82a197
Rjtsahu <sahurjt@gmail.com>
2019-08-25 00:42:52 -0700

    minor refaccoring

--- Core/Message/RequestQueryCollection.cs
+++ Core/Message/RequestQueryCollection.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
 namespace Sahurjt.Signalr.Dashboard.Core.Message
 {
