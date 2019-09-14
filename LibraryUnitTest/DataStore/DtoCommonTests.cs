b1170fff50d1fe4b6f6050d1de95ec4bfd2ccaf8
rjtsahu <sahurjt@gmail.com>
2019-09-15 02:14:37 +0530

    added dto extension test

--- LibraryUnitTest/DataStore/DtoCommonTests.cs
+++ LibraryUnitTest/DataStore/DtoCommonTests.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Sahurjt.Signalr.Dashboard.Core;
 using Sahurjt.Signalr.Dashboard.DataStore;
 using Sahurjt.Signalr.Dashboard.DataStore.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibraryUnitTest.DataStore
@@ -24,11 +28,16 @@ namespace LibraryUnitTest.DataStore
 
             sqlOperationMock.Setup(s => s.Execute(It.IsAny<ExecuteSqlQuery>(), It.IsAny<object[]>())).Returns(1);
             sqlOperationMock.Setup(s => s.ExecuteAsync(It.IsAny<ExecuteSqlQuery>(), It.IsAny<object[]>())).Returns(Task.FromResult(1));
+            sqlOperationMock.Setup(s => s.Select<HubDataDto>(It.IsAny<SelectSqlQuery>(), It.IsAny<object[]>())).Returns(new HubDataDto { Id = 1 });
+            sqlOperationMock.Setup(s => s.SelectMultiple<HubDataDto>(It.IsAny<SelectSqlQuery>(), It.IsAny<object[]>())).Returns(new List<HubDataDto> {
+                new HubDataDto{ Id = 1} , new HubDataDto{ Id =2}
+            });
 
             sqlOperationFailureMock = new Mock<ISqlOperation>();
 
             sqlOperationFailureMock.Setup(s => s.Execute(It.IsAny<ExecuteSqlQuery>(), It.IsAny<object[]>())).Returns(0);
             sqlOperationFailureMock.Setup(s => s.ExecuteAsync(It.IsAny<ExecuteSqlQuery>(), It.IsAny<object[]>())).Returns(Task.FromResult(0));
+
 
             hubDataDto = new HubDataDto();
             requestDto = new RequestDto();
@@ -77,6 +86,110 @@ namespace LibraryUnitTest.DataStore
 
             Assert.IsFalse(sessionReportDto.Save(sqlOperationFailureMock.Object));
             Assert.IsFalse(sessionReportDto.SaveAsync(sqlOperationFailureMock.Object).Result);
+        }
+
+        //// DTO extension tests
+
+        [TestMethod]
+        public void Test_AsBoolean()
+        {
+            var result = hubDataDto.AsBoolean("true");
+            Assert.IsTrue(result);
+            result = hubDataDto.AsBoolean("false");
+            Assert.IsFalse(result);
+            result = hubDataDto.AsBoolean("invalid_data");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_DateTimeConversion()
+        {
+            var date = DateTime.Now;
+            var result = hubDataDto.AsDateTime(hubDataDto.AsEpoch(date));
+            Assert.AreEqual(date.Date, result.Date);
+            Assert.AreEqual(date.Day, result.Day);
+            Assert.AreEqual(date.Hour, result.Hour);
+            Assert.AreEqual(date.Minute, result.Minute);
+            Assert.AreEqual(date.Second, result.Second);
+        }
+
+        [TestMethod]
+        public void Test_CsvAsList()
+        {
+            var result = hubDataDto.CsvAsList(null);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+
+            result = hubDataDto.CsvAsList("test,mock");
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+
+        }
+
+        [TestMethod]
+        public void Test_GetAll_Success()
+        {
+            DashboardGlobal.ServiceResolver.Replace<ISqlOperation, ISqlOperation>(sqlOperationMock.Object);
+
+            var result = hubDataDto.GetAll();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.ToList().Count);
+        }
+
+
+        [TestMethod]
+        public void Test_GetSingle_Success()
+        {
+            DashboardGlobal.ServiceResolver.Replace<ISqlOperation, ISqlOperation>(sqlOperationMock.Object);
+
+            var result = hubDataDto.GetSingle(1);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Id);
+        }
+
+        [TestMethod]
+        public void Test_GetAll_Failure()
+        {
+            var dummyDto = new DummyDto();
+
+            DashboardGlobal.ServiceResolver.Replace<ISqlOperation, ISqlOperation>(sqlOperationMock.Object);
+
+            Assert.ThrowsException<NotImplementedException>(
+                () => dummyDto.GetAll()
+             );
+        }
+
+        [TestMethod]
+        public void Test_GetSingle_Failure()
+        {
+            var dummyDto = new DummyDto();
+
+            DashboardGlobal.ServiceResolver.Replace<ISqlOperation, ISqlOperation>(sqlOperationMock.Object);
+
+            Assert.ThrowsException<NotImplementedException>(
+                () => dummyDto.GetSingle(1)
+             );
+        }
+
+
+        /// <summary>
+        /// stub table to unit test extension method GetSingle,GetAll failure case.
+        /// </summary>
+        class DummyDto : IDataTableObject
+        {
+            public string TableName => "DummyTable";
+
+            public bool Save(ISqlOperation sqlOperation)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<bool> SaveAsync(ISqlOperation sqlOperation)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
--- LibraryUnitTest/DataStore/SqlOperationExceptionTests.cs
+++ LibraryUnitTest/DataStore/SqlOperationExceptionTests.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sahurjt.Signalr.Dashboard.DataStore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibraryUnitTest.DataStore
 {
